4576a0de8ee1fcac4be78ea99fafd0505b3cf41c
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-11-11 14:48:34 +0100

    рефакторинг кода Searchbar используя React-хуки

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,56 +1,103 @@
-import React from 'react';
-import { Component } from 'react';
+import { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { toast } from 'react-toastify';
 import styles from '../Searchbar/Searchbar.module.css';
 
-export default class Searchbar extends Component {
-  state = {
-    searchName: '',
+export default function Searchbar({ onSubmit }) {
+  const [searchName, setSearchName] = useState('');
+
+  const handleChange = e => {
+    setSearchName(e.currentTarget.value.toLowerCase());
+    // this.setState({
+    //   searchName: e.currentTarget.value.toLowerCase(),
+    // });
   };
 
-  handleChange = e => {
-    this.setState({
-      searchName: e.currentTarget.value.toLowerCase(),
-    });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.searchName.trim() === '') {
+    if (searchName.trim() === '') {
       //   alert('Enter a search name')
       toast.warn('Enter a search name');
       return;
     }
-    this.props.onSubmit(this.state.searchName);
-    this.resetForm();
+    onSubmit(searchName);
+    resetForm();
   };
 
-  resetForm = () => {
-    this.setState({ searchName: '' });
+  const resetForm = () => {
+    setSearchName('');
   };
 
-  render() {
-    return (
-      <header className={styles.Searchbar}>
-        <form onSubmit={this.handleSubmit} className={styles.SearchForm}>
-          <button type="submit" className={styles.SearchForm_button}>
-            <AiOutlineSearch />
-            <span className={styles.SearchFormButton_label}>Search</span>
-          </button>
+  return (
+    <header className={styles.Searchbar}>
+      <form onSubmit={handleSubmit} className={styles.SearchForm}>
+        <button type="submit" className={styles.SearchForm_button}>
+          <AiOutlineSearch />
+          <span className={styles.SearchFormButton_label}>Search</span>
+        </button>
 
-          <input
-            className={styles.SearchForm_input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.searchName}
-            onChange={this.handleChange}
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          className={styles.SearchForm_input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchName}
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
 }
+
+// export default class Searchbar extends Component {
+//   state = {
+//     searchName: '',
+//   };
+
+//   handleChange = e => {
+//     this.setState({
+//       searchName: e.currentTarget.value.toLowerCase(),
+//     });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     if (this.state.searchName.trim() === '') {
+//       //   alert('Enter a search name')
+//       toast.warn('Enter a search name');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.searchName);
+//     this.resetForm();
+//   };
+
+//   resetForm = () => {
+//     this.setState({ searchName: '' });
+//   };
+
+//   render() {
+//     return (
+//       <header className={styles.Searchbar}>
+//         <form onSubmit={this.handleSubmit} className={styles.SearchForm}>
+//           <button type="submit" className={styles.SearchForm_button}>
+//             <AiOutlineSearch />
+//             <span className={styles.SearchFormButton_label}>Search</span>
+//           </button>
+
+//           <input
+//             className={styles.SearchForm_input}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.searchName}
+//             onChange={this.handleChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
