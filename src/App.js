9d1e91d40edf7828d442f28d74ba7a4951a31b70
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-11-17 13:11:06 +0100

    чистит код

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Loader from 'react-loader-spinner';
 import searchApi from './services/api';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 import Button from './components/Button/Button';
-import { Modal } from './components/Modal/Modal';
+import Modal from './components/Modal/Modal';
 
 export default function App() {
   const [searchName, setSearchName] = useState('');
@@ -32,12 +32,10 @@ export default function App() {
         setSearchInfo(prevState => [...prevState, ...data.hits]);
         setStatus('resolved');
         scrollFunction();
-        console.log(searchPage);
+        // console.log(searchPage);
       })
       .catch(error => setError(error.message));
     return;
-
-    // return () => {setSearchPage(1)}
   }, [searchName, searchPage]);
 
   const handleFormSubmit = searchName => {
@@ -59,30 +57,32 @@ export default function App() {
 
   const loadMoreFunction = () => {
     setSearchPage(prevState => prevState + 1);
+  };
 
-    // setStatus('pending');
-    //   searchApi
-    //     .fetchImage(searchName, searchPage)
-    //     .then(data => {
-    //       setSearchInfo(prevState => [...prevState, data.hits]);
-    //       console.log(searchInfo);
-    //       setStatus('resolved');
+  const onCloseModal = () => {
+    setShowModal(false);
+  };
 
-    //        setSearchPage(prevState=>prevState + 1)
-    //       console.log(searchPage);
-    //       console.log('лм фетч');
-    //     })
-    //     .catch(error => console.log(error))
+  const onOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const onImgClick = e => {
+    // console.log(e.target.dataset);
+    if (e.target.nodeName !== 'IMG') {
+      return;
+    }
+    setLargeImgUrl(e.target.dataset.url);
   };
 
   return (
     <div className={styles.App}>
       <Searchbar onSubmit={handleFormSubmit} />
-      {/* {showModal && (
-          <Modal onCloseModal={this.onCloseModal}>
-            <img src={largeImgUrl} alt="" />
-          </Modal>
-        )} */}
+      {showModal && (
+        <Modal onCloseModal={onCloseModal}>
+          <img src={largeImgUrl} alt="" />
+        </Modal>
+      )}
       <ToastContainer position="top-center" autoClose={2000} />
       {status === 'idle' && <div>Enter something</div>}
       {status === 'pending' && (
@@ -94,172 +94,15 @@ export default function App() {
           timeout={3000}
         />
       )}
-      {/* {status === 'rejected' && <h1>{error.message}</h1>} */}
       {status === 'rejected' && <h1>{error}</h1>}
       <ImageGallery
         images={searchInfo}
-        // onClick={this.onOpenModal}
-        // onImgClick={this.onImgClick}
+        onClick={onOpenModal}
+        onImgClick={onImgClick}
       />
       {status === 'resolved' && searchInfo.length > 0 && (
-        <>
-          {/* <ImageGallery
-            images={searchInfo}
-            // onClick={this.onOpenModal}
-            // onImgClick={this.onImgClick}
-          /> */}
-          <Button onClick={handleClick} />
-        </>
+        <Button onClick={handleClick} />
       )}
     </div>
   );
 }
-
-// const fetchFunction = () => {
-//  console.log('fetch');
-//  return searchApi
-//    .fetchImage(searchName, searchPage)
-//    .then(searchInfo => {
-//      setSearchInfo([...searchInfo, ...searchInfo.hits]);
-//      setStatus('resolved');
-//      setSearchPage(prevState => prevState + 1)
-//    })
-//    .catch(error =>
-//      setError(error),
-//      setStatus('rejected')
-//     );
-//       // return()=>{console.log('stop')
-// };
-
-//  .then(searchInfo => {
-//    setSearchInfo([...searchInfo, ...searchInfo.hits]);
-//    setStatus('resolved');
-//    setSearchPage(prevState => prevState + 1)
-//  })
-// .catch(error => {
-//   console.log(error);
-//  setError(error),
-//  setStatus('rejected')});
-
-// return()=>{console.log('stop');}
-// }
-// },
-//     [searchName, searchPage]))
-// useEffect(() => {
-//   if (searchName === '') {
-//     return
-
-//   setSearchPage(1);
-//   setStatus('pending');
-//   setSearchInfo([]);
-//   fetchFunction(searchName, searchPage);
-
-//   // return()=>{console.log('stop');}
-//    },
-//     [searchName, searchPage])
-
-// class App extends React.Component {
-//   state = {
-//     searchName: '',
-//     searchInfo: [],
-//     error: null,
-//     status: 'idle',
-//     searchPage: 1,
-//     showModal: false,
-//     largeImgUrl: '',
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     if (this.state.searchInfo.length > 12) {
-//       window.scrollTo({
-//         top: document.documentElement.scrollHeight,
-//         behavior: 'smooth',
-//       });
-//     }
-
-//     if (prevState.searchName !== this.state.searchName) {
-//       this.setState({ searchPage: 1 });
-//       this.setState({ status: 'pending' });
-//       this.setState({ searchInfo: [] });
-//       this.fetchFunction();
-//     }
-//     return;
-//   }
-
-//   handleFormSubmit = searchName => {
-//     this.setState({ searchName });
-//   };
-
-//   handleClick = () => {
-//     this.fetchFunction();
-//     // console.log(this.state.searchInfo.length);
-//   };
-
-//   fetchFunction = () => {
-//     return searchApi
-//       .fetchImage(this.state.searchName, this.state.searchPage)
-//       .then(searchInfo => {
-//         this.setState({
-//           searchInfo: [...this.state.searchInfo, ...searchInfo.hits],
-//           status: 'resolved',
-//         });
-//         this.setState(prevState => ({ searchPage: prevState.searchPage + 1 }));
-//       })
-//       .catch(error => this.setState({ error, status: 'rejected' }));
-//   };
-
-//   onCloseModal = () => {
-//     this.setState({ showModal: false });
-//   };
-
-//   onOpenModal = () => {
-//     this.setState({ showModal: true });
-//   };
-
-//   onImgClick = e => {
-//     console.log(e.target.dataset);
-//     if (e.target.nodeName !== 'IMG') {
-//       return;
-//     }
-//     this.setState({ largeImgUrl: e.target.dataset.url });
-//   };
-
-//   render() {
-//     const { searchInfo, error, status, showModal, largeImgUrl } = this.state;
-
-//     return (
-//       <div className={styles.App}>
-//         <Searchbar onSubmit={this.handleFormSubmit} />
-//         {showModal && (
-//           <Modal onCloseModal={this.onCloseModal}>
-//             <img src={largeImgUrl} alt="" />
-//           </Modal>
-//         )}
-//         <ToastContainer position="top-center" autoClose={2000} />
-//         {status === 'idle' && <div>Enter something</div>}
-//         {status === 'pending' && (
-//           <Loader
-//             type="BallTriangle"
-//             color="#00BFFF"
-//             height={100}
-//             width={100}
-//             timeout={3000}
-//           />
-//         )}
-//         {status === 'rejected' && <h1>{error.message}</h1>}
-//         {status === 'resolved' && searchInfo.length > 0 && (
-//           <>
-//             <ImageGallery
-//               images={searchInfo}
-//               onClick={this.onOpenModal}
-//               onImgClick={this.onImgClick}
-//             />
-//             <Button onClick={this.handleClick} />
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,39 +1,33 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import styles from '../Modal/Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscape);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscape);
-  }
+export default function Modal({ onCloseModal, children }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscape);
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  });
 
-  handleEscape = e => {
+  const handleEscape = e => {
     let condition = e.code === 'Escape';
     if (condition) {
-      this.props.onCloseModal();
+      onCloseModal();
     }
     console.log(e.code === 'Escape');
   };
 
-  handleClose = e => {
+  const handleClose = e => {
     if (e.currentTarget === e.target) {
-      this.props.onCloseModal();
+      onCloseModal();
     }
   };
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <div className={styles.Overlay} onClick={this.handleClose}>
-        <div className={styles.Modal}>
-          {children}
 
-          {/* <img src="" alt="" /> */}
-        </div>
-      </div>,
-      document.getElementById('modalRoot'),
-    );
-  }
+  return createPortal(
+    <div className={styles.Overlay} onClick={handleClose}>
+      <div className={styles.Modal}>{children}</div>
+    </div>,
+    document.getElementById('modalRoot'),
+  );
 }
